2834e898140cd74ba875431b78ce08f3b1a7bcbc
Avery Burne <burneav@gmail.com>
2020-07-21 22:09:17 -0400

    attempt to get handlebars file to read map-info value

--- assets/scripts/hammockSpots/ui.js
+++ assets/scripts/hammockSpots/ui.js
@@ -3,14 +3,14 @@
 const create = function (data) {
   const pointers = []
   console.log(data.hammock)
-  console.log(pointers)
-  for (let i = 0; i < data.hammock.length; i++) {
-    if (data.hammockSpot[i].lat !== null && data.hammockSpot[i].lng !== null) {
-      if (data.hammockSpot[i].lat !== undefined && data.hammockSpot[i].lng !== undefined) {
-        pointers.push({ lat: data.hammockSpot[i].lat, lng: data.hammockSpot[i].lng })
+  for (let i = 0; i < 1; i++) {
+    if (data.hammock.lat !== null && data.hammock.lng !== null) {
+      if (data.hammock.lat !== undefined && data.hammock.lng !== undefined) {
+        pointers.push({ lat: data.hammock.lat, lng: data.hammock.lng })
       }
     }
   }
+  console.log(pointers)
   $('#map-info').val({ pointers: pointers })
   console.log($('#map-info').val)
 }
