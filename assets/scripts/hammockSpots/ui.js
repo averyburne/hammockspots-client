166585a69ca34bbca13830011068f8a42a5dee2f
Avery Burne <burneav@gmail.com>
2020-08-09 21:17:54 -0400

    All returned values appear on map
    removed console.log in ui file for clearer console

--- assets/scripts/hammockSpots/ui.js
+++ assets/scripts/hammockSpots/ui.js
@@ -6,18 +6,15 @@ const create = function (data) {
 
 const onIndexSuccess = function (data) {
   const pointers = []
-  console.log(data.hammockSpot)
   for (let i = 0; i < data.hammockSpot.length; i++) {
     if (data.hammockSpot[i].lat !== null && data.hammockSpot[i].lng !== null) {
       if (data.hammockSpot[i].lat !== undefined && data.hammockSpot[i].lng !== undefined) {
         console.log(data.hammockSpot[i])
         pointers.push({ lat: data.hammockSpot[i].lat, lng: data.hammockSpot[i].lng })
       }
-    }f
+    }
   }
-  console.log(pointers)
   $('#map-info').val({ pointers: pointers })
-  console.log($('#map-info').val)
 }
 
 const onIndexFailure = function (data) {
