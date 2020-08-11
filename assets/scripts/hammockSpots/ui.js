846a2739a26725e8614c92299ec9acf30b5612f0
Avery Burne <burneav@gmail.com>
2020-08-11 18:16:07 -0400

    Added list of locations on map at bottom of page
    Getting locations on map now also loads all the locations in a list
    The list contains the name, Latitude, and Longitude

--- assets/scripts/hammockSpots/ui.js
+++ assets/scripts/hammockSpots/ui.js
@@ -1,10 +1,17 @@
 'use strict'
+const showHammockSpotsTemplate = require('../templates/hammockSpot.handlebars')
 
 const create = function (data) {
 
 }
 
 const onIndexSuccess = function (data) {
+  const showHammockSpotsHtml = showHammockSpotsTemplate({ hammockSpots: data.hammockSpot })
+  $('.content').html(showHammockSpotsHtml)
+  if(data.hammockSpot.length === 0) {
+    console.log('theres no locations')
+  }
+
   const pointers = []
   for (let i = 0; i < data.hammockSpot.length; i++) {
     if (data.hammockSpot[i].lat !== null && data.hammockSpot[i].lng !== null) {
